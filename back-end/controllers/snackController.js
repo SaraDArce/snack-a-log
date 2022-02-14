d96a9f6fb5a8b4837473b0be76a2d85f34ea4f66
Milton Clark <kingcoder247@gmail.com>
2022-02-14 10:34:37 -0500

    Updated snackController and queries snacks file

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const snacks = express.Router();
 
-const { getAllSnacks, } = require("../queries/snacks.js");
+const { getAllSnacks, getOneSnack } = require("../queries/snacks.js");
 
 snacks.get("/", async (req, res) => {
     try{
         const allSnacks = await getAllSnacks();
         if(allSnacks[0]){
-            res.status(200).json(allSnacks);
+            res.status(200).json({success: true, payload: allSnacks});
         } else{
-            res.status(500).json({error: "No snacks were returned from db"});
+            res.status(404).json({error: "No snacks were returned from db"});
         }
     } catch(err){
-        console.log(err);
+       return err;
+    }
+});
+
+snacks.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        const snack = await getOneSnack(id);
+        if(snack.id){
+            res.status(200).json({success: true, payload: snack});
+        } else{
+            res.status(404).json({error: "Snack not returned from db"});
+        }
+    } catch(err){
+        return err;
     }
 });
 
